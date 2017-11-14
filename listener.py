2683d5fcdfba72acab3836b38b6f2f116912cf06
AJ Bowen <aj@soulshake.net>
2017-11-14 11:11:30 -0800

    Switching computers

--- listener.py
+++ listener.py
@@ -12,5 +12,18 @@ def print_results(data):
     for k in data:
         print("{}: {}".format(k, data[k]))
 
+def write_results(data):
+    with open("results.json", "a") as f:
+        existing = f.read()
+    current = json.loads(existing)
+    instance_id = data["instance_id"]
+    del(data["instance_id"])
+    if instance_id not in existing:
+        existing[instance_id] = {}
+    for k in data:
+        existing[instance_id][k] = data[k]
+    with open("results.json", "a") as f:
+        f.write(existing)
+
 if __name__ == "__main__":
     app.run(debug=True)
